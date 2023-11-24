88d21ed5fd96c8903dfa0dcbfafd58e3697fc888
movieminer <thijsg.dejong@gmail.com>
2023-11-24 13:24:58 +0100

    Add translation functionality using external API

--- project/data.py
+++ project/data.py
@@ -1,7 +1,6 @@
 import json
 import pandas as pd
-from google.cloud import translate_v2
-
+import requests
 
 def get_relevant_video_data(path):
   with open(path) as f:
@@ -52,6 +51,17 @@ def save_pd_as_csv(data, path):
 
 
 def translate_comment(comment):
-  translate_client = translate_v2.Client()
-  result = translate_client.translate(comment, target_language='en', source_language='nl')
-  return result
+  
+  api_url = "http://mymemory.translated.net/api/get?q={}&langpair={}|{}".format(comment, "nl", "en")
+  hdrs = {
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+      'Accept-Encoding': 'none',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Connection': 'keep-alive'}
+  response = requests.get(api_url, headers=hdrs)
+  response_json = json.loads(response.text)
+  translation = response_json["responseData"]["translatedText"]
+
+  return translation
