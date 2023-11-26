15cd4130a21849f2375c0c1a131dc9106c172c9b
movieminer <thijsg.dejong@gmail.com>
2023-11-26 13:08:46 +0100

    style

--- project/data.py
+++ project/data.py
@@ -51,23 +51,24 @@ def save_pd_as_csv(data, path):
   data.to_csv(path, index=False)
 
 
-# def translate_comment(comment):
-#   api_url = "http://mymemory.translated.net/api/get?q={}&langpair={}|{}&de={}".format(comment, "nl", "en", "gamersgamingrandom@gmail.com")
-#   hdrs = {
-#       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-#       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-#       'Accept-Encoding': 'none',
-#       'Accept-Language': 'en-US,en;q=0.8',
-#       'Connection': 'keep-alive'}
-#   response = requests.get(api_url, headers=hdrs)
-#   response_json = json.loads(response.text)
-#   translation = response_json["responseData"]["translatedText"]
+def translate_comment_google_api(comment):
+  api_url = "http://mymemory.translated.net/api/get?q={}&langpair={}|{}&de={}".format(comment, "nl", "en", "gamersgamingrandom@gmail.com")
+  hdrs = {
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+      'Accept-Encoding': 'none',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Connection': 'keep-alive'}
+  response = requests.get(api_url, headers=hdrs)
+  response_json = json.loads(response.text)
+  translation = response_json["responseData"]["translatedText"]
 
-#   print(comment)
-#   print(translation + "\n")
+  print(comment)
+  print(translation + "\n")
 
-#   return translation
+  return translation
+
 
 translator = GoogleTranslator(source='auto', target='en')
 
