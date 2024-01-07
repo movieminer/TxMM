c4f438d43857b005a9e562911d82db03e5467584
movieminer <thijsg.dejong@gmail.com>
2024-01-07 19:30:55 +0100

    Refactor preprocess.py to drop duplicates, sort by publishedAt, and filter comments by month

--- project/preprocess.py
+++ project/preprocess.py
@@ -11,5 +11,16 @@ with open("data/csv/comments/comments.csv", "r") as f:
 print(comments.shape)
 comments = comments.drop_duplicates()
 print(comments.shape)
-comments = comments.drop_duplicates(subset=['videoId','textDisplay', 'authorDisplayName'])
+comments = comments.drop_duplicates(subset=['textDisplay', 'authorDisplayName'])
 print(comments.shape)
+comments = comments.sort_values(by=['publishedAt'], ascending=True)
+print(comments.head(10))
+
+# Return subset of comments from specific months
+def get_comments_by_month(comments, month):
+  return comments[comments['publishedAt'].str.contains(f'2023-{month}-')]
+
+months = ['07', '08', '09', '10', '11']
+
+for month in months:
+  print(get_comments_by_month(comments, month).shape)
